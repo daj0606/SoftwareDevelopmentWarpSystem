6b2789c3f660af4ac15ecc88412518a0b8631a2e
djackson11 <djackson11@uiowa.edu>
2025-09-19 22:48:51 -0500

    Add JavaDoc comments for Warp, Channels, ChennelAnalysis, and ProgramVisualization

--- src/edu/uiowa/cs/warp/Channels.java
+++ src/edu/uiowa/cs/warp/Channels.java
@@ -1,78 +1,130 @@
 package edu.uiowa.cs.warp;
 
-
 import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
+ * Tracks which communication channels are free in each time slot of a WARP schedule.
+ * 
  * @author sgoddard
  * @version 1.4
  *
  */
 public class Channels {
+	
+	/**
+	 * A set of channel identifiers (as strings) for a single time slot.
+	 */
+	private class ChannelSet extends HashSet<String> {
+		private static final long serialVersionUID = 6725256944325470867L;
 
+		/** Creates an empty channel set. */
+		private ChannelSet() {
+			super();
+		}
 
-  private class ChannelSet extends HashSet<String> {
-    private static final long serialVersionUID = 6725256944325470867L;
+		/**
+		 * Creates a channel set pre-populated with all channel IDs
+		 * from 0 to nChannels-1
+		 * 
+		 * @param nchannels total number of channels in the system
+		 */
+		private ChannelSet(Integer nChannels) {
+			super();
+			for (int i = 0; i < nChannels; i++) { // ASSUMES channels range from 0 to nChannels-1
+				this.add(String.valueOf(i));
+			}
+		}
+	}
 
-    // default constructor
-    private ChannelSet() {
-      super();
-    }
+	Integer nChannels; // size of the full set of channels
+	Boolean verbose;
+	ArrayList<ChannelSet> channelsAvailable; // ArrayList to hold channels available in each time slot
 
-    private ChannelSet(Integer nChannels) {
-      super();
-      for (int i = 0; i < nChannels; i++) { // ASSUMES channels range from 0 to nChannels-1
-        this.add(String.valueOf(i));
-      }
-    }
-  }
+	/**
+	 * Creates a channel manager with the given number of channels and a verbosity flag.
+	 * Initially, no time slots exist. Call addNewChannelSet() to start adding time slots.
+	 * 
+	 * @param nChannels number of channels
+	 * @param verbose whether to enable verbose behavior
+	 */
+	Channels(Integer nChannels, Boolean verbose) {
+		this.nChannels = nChannels;
+		this.verbose = verbose;
+		this.channelsAvailable = new ArrayList<ChannelSet>();
+	}
 
+	/**
+	 * Returns copy of the channel set for the given time slot.
+	 * 
+	 * @param timeSlot index of the time slot
+	 * @return a new HashSet containing the available channels in that slot.
+	 */
+	public HashSet<String> getChannelSet(Integer timeSlot) {
+		/* get the channel set for this timeSlot */
+		HashSet<String> channelSet = new HashSet<String>(channelsAvailable.get(timeSlot));
+		return channelSet;
+	}
 
-  Integer nChannels; // size of the full set of channels
-  Boolean verbose;
-  ArrayList<ChannelSet> channelsAvailable; // ArrayList to hold channels available in each time slot
+	/**
+	 * Appends a new time slot to the schedule. The new slot starts with all
+	 * channels marked as available.
+	 */
+	public void addNewChannelSet() {
+		var channels = new ChannelSet(nChannels);
+		channelsAvailable.add(channels);
+	}
 
-  Channels(Integer nChannels, Boolean verbose) {
-    this.nChannels = nChannels;
-    this.verbose = verbose;
-    this.channelsAvailable = new ArrayList<ChannelSet>();
-  }
+	/**
+	 * Checks whether the given time slot has no channels available.
+	 * 
+	 * @param timeSlot idnex of the time slot
+	 * @return true if the set is empty, false otherwise
+	 */
+	public Boolean isEmpty(int timeSlot) {
+		ChannelSet channelSet = channelsAvailable.get(timeSlot); // get the channel set for this
+																	// timeSlot
+		return channelSet.isEmpty(); // returns true channel set is empty and false if not
+	}
 
-  public HashSet<String> getChannelSet(Integer timeSlot) {
-    /* get the channel set for this timeSlot */
-    HashSet<String> channelSet = new HashSet<String>(channelsAvailable.get(timeSlot));
-    return channelSet;
-  }
+	/**
+	 * Removes a channel from the availability set of the given time slot,
+	 * marking it as occupied by a transmission
+	 * 
+	 * @param timeSlot index of the time lot
+	 * @param channel channel ID
+	 * @return true if the channel was present and removed, false otherwise
+	 */
+	public Boolean removeChannel(int timeSlot, String channel) {
+		Boolean result;
+		ChannelSet channels = channelsAvailable.get(timeSlot);
+		result = channels.remove(channel);
+		return result;
+	}
 
-  public void addNewChannelSet() {
-    var channels = new ChannelSet(nChannels);
-    channelsAvailable.add(channels);
-  }
+	/**
+	 * Adds a channel back into the availability set for the given time slot, 
+	 * marking it as free again.
+	 * 
+	 * @param timeSlot index of the time slot
+	 * @param channel channel ID
+	 * @return true if the channel was not already present and is now added,
+	 * 		   false if it was already present
+	 */
+	public Boolean addChannel(int timeSlot, String channel) {
+		Boolean result;
+		ChannelSet channels = channelsAvailable.get(timeSlot); // get a pointer to the channel set
+		result = channels.add(channel); // add the channel, returning the result
+		return result;
+	}
 
-
-  public Boolean isEmpty(int timeSlot) {
-    ChannelSet channelSet = channelsAvailable.get(timeSlot); // get the channel set for this
-                                                             // timeSlot
-    return channelSet.isEmpty(); // returns true channel set is empty and false if not
-  }
-
-  public Boolean removeChannel(int timeSlot, String channel) {
-    Boolean result;
-    ChannelSet channels = channelsAvailable.get(timeSlot);
-    result = channels.remove(channel);
-    return result;
-  }
-
-  public Boolean addChannel(int timeSlot, String channel) {
-    Boolean result;
-    ChannelSet channels = channelsAvailable.get(timeSlot); // get a pointer to the channel set
-    result = channels.add(channel); // remove the channel, returning the result
-    return result;
-  }
-
-  public Integer getNumChannels() {
-    return nChannels;
-  }
+	/**
+	 * Returns the total number of channels in the system.
+	 * 
+	 * @return number of channels
+	 */
+	public Integer getNumChannels() {
+		return nChannels;
+	}
 
 }
